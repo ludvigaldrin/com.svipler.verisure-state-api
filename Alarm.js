59fa4d2c2b3a09eab223fccdca9f05e7569b4a56
Ludvig Aldrin <ludvig.aldrin@gmail.com>
2023-03-19 12:19:34 +0100

    fix

--- Alarm.js
+++ Alarm.js
@@ -30,21 +30,85 @@ class Alarm {
 
   getCurrentInstallation(callback) {
     const request = {
-      operationName: 'ArmState',
-      variables: { giid: this.installation.giid },
-      query: `query ArmState($giid: String!) {
-        installation(giid: $giid) {
-          armState {
-            type
-            statusType
-            date
-            name
-            changedVia
+      operationName: 'Overview',
+      query: `query Overview($giid: String!) {
+          installation(giid: $giid) {
+            alias
+            locale
+            climates {
+              device {
+                deviceLabel
+                area
+                gui {
+                  label
+                  __typename
+                }
+                __typename
+              }
+              humidityEnabled
+              humidityTimestamp
+              humidityValue
+              temperatureTimestamp
+              temperatureValue
+              thresholds {
+                aboveMaxAlert
+                belowMinAlert
+                sensorType
+                __typename
+              }
+              __typename
+            }
+            armState {
+              type
+              statusType
+              date
+              name
+              changedVia
+              __typename
+            }
+            doorWindows {
+              device {
+                deviceLabel
+                area
+                gui {
+                  support
+                  label
+                  __typename
+                }
+                __typename
+              }
+              type
+              state
+              wired
+              reportTime
+              __typename
+            }
+            smartplugs {
+              device {
+                deviceLabel
+                area
+                gui {
+                  support
+                  label
+                  __typename
+                }
+                __typename
+              }
+              currentState
+              icon
+              isHazardous
+              __typename
+            }
+            doorlocks {
+              device {
+                area
+                deviceLabel
+                __typename
+              }
+            }
             __typename
           }
-          __typename
-        }
-      }`,
+        }`
     }
 
     this.installation
